66e0de9150776a9205dc1cb975042d8af5bc92e8
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-06 17:48:41 +0530

    data fetching

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -4,12 +4,35 @@ import { useEffect, useState } from "react";
 const Meals = () => {
   const [loadedMeals, setLoadedMeals] = useState([]);
 
+  // here i m using browser built in method fetch for https requests
+
+  // useEffect(() => {
+  //   try {
+  //     async function getMeals() {
+  //       const response = await fetch("http://localhost:3000/meals");
+  //       const meals = await response.json();
+  //       setLoadedMeals(meals);
+  //     }
+  //     getMeals();
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, []);
+
+  // here i m using external axios library for https request
+  // its preferred because you don't have to convert data in json format
+  // it easy to use
+
   useEffect(() => {
-    async function getMeals() {
-      const response = await Axios.get("http://localhost:3000/meals");
-      setLoadedMeals(response.data);
+    try {
+      async function getMeals() {
+        const response = await Axios.get("http://localhost:3000/meals");
+        setLoadedMeals(response.data);
+      }
+      getMeals();
+    } catch (error) {
+      console.log(error);
     }
-    getMeals();
   }, []);
 
   console.log("meals Data=>", loadedMeals);
