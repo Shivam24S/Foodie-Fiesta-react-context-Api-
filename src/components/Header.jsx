12b801a6bdb9cfd5d8945f63fbe7f298b575c10a
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-07 10:27:16 +0530

    Button Component Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
           <h1>Foodie</h1>
         </div>
         <nav>
-          <button>Cart (0)</button>
+          <Button textOnly>Cart (0)</Button>
         </nav>
       </header>
     </>
